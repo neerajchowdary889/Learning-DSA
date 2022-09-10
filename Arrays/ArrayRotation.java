5c3b77124ab725d40ecc8e776157232de0149503
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-09-10 23:52:43 +0530

    Array Rotation

--- Arrays/ArrayRotation.java
+++ Arrays/ArrayRotation.java
@@ -6,5 +6,22 @@ public class ArrayRotation {
         int len = array.length;
         // rotations is d
         int d = 3;
+
+        // Iteration start
+
+        int j =0;
+        int[] temp = new int[d];
+        for (j = 0; j <= d - 1; j++) {
+            temp[j] = array[j];
+        }
+
+        int tempint = 0;
+        for (int k = d; k<=len; k++){
+            array[tempint] = array[k];
+        }
+
+        for (int i = 0; i<=2 ; i++){
+
+        }
     }
 }
