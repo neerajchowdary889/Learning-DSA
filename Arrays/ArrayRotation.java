d39a9e4236c25978d0eef11e6a9a4a5ec97e0304
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-09-10 23:19:08 +0530

    Array Rotation

---
+++ Arrays/ArrayRotation.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ArrayRotation {
+    public static void main (String[]args){
+        int[] array = {1,2,3,4,5,6};
+        int len = array.length;
+        // rotations is d
+        int d = 3;
+    }
+}
