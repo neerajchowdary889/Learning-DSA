34dd2c9876a107ac37ccb81522d5972dff22acc2
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-09-11 00:13:49 +0530

    Array Rotation

--- Arrays/ArrayRotation.java
+++ Arrays/ArrayRotation.java
@@ -2,10 +2,11 @@ package Arrays;
 
 public class ArrayRotation {
     public static void main (String[]args){
-        int[] array = {1,2,3,4,5,6};
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
         int len = array.length;
+
         // rotations is d
-        int d = 3;
+        int d = 5;
 
         // Iteration start
 
@@ -16,12 +17,20 @@ public class ArrayRotation {
         }
 
         int tempint = 0;
-        for (int k = d; k<=len; k++){
+        for (int k = d; k<=len-1; k++){
             array[tempint] = array[k];
+            tempint++;
         }
 
-        for (int i = 0; i<=2 ; i++){
+        int tempint2 = 0;
+        for (int i = len-d; i<=len-1 ; i++){
+            array[i] = temp[tempint2];
+            tempint2++;
+        }
 
+        for (int z = 0; z <=len-1; z++){
+            System.out.println(array[z]);
         }
     }
 }
+// Output: 6, 7, 8, 9, 10, 1, 2, 3, 4, 5
