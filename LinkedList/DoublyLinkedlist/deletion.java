4ef031a97a2c1a9962b4f1ff4a96bfc17483fcd6
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-09-13 00:13:27 +0530

    DoublyLinkedlist

--- LinkedList/DoublyLinkedlist/deletion.java
+++ LinkedList/DoublyLinkedlist/deletion.java
@@ -1,8 +1,8 @@
 package LinkedList.DoublyLinkedlist;
 
-public class DoublyLinkedlist {
-     static Node head = null;
-     static Node tail = null;
+public class deletion {
+    static Node head = null;
+    static Node tail = null;
     static class Node{
         int data;
         Node next;
@@ -11,21 +11,6 @@ public class DoublyLinkedlist {
             this.data = data;
             this.next = null;
             this.prev = null;
-        }
-    }
-    public static void InsertatStart(int data){
-        Node new_node = new Node(data);
-        if(head == null){
-            head = new_node;
-            tail = new_node;
-            head.next = null;
-            head.prev = null;
-        }
-        else{
-            head.prev = new_node;
-            new_node.next = head;
-            new_node.prev = null;
-            head = new_node;
         }
     }
     public static void Insert(int data){
@@ -53,9 +38,11 @@ public class DoublyLinkedlist {
         else{
             Node curr = head;
             System.out.println("In Forward Direction: ");
+            int i = 0;
             while(curr != null){
                 System.out.print(curr.data + " ");
                 curr = curr.next;
+                i++;
             }
             Node Rcurr = tail;
             System.out.println("\nIn Reverse Direction: ");
@@ -63,18 +50,57 @@ public class DoublyLinkedlist {
                 System.out.print(Rcurr.data + " ");
                 Rcurr = Rcurr.prev;
             }
+            System.out.println("\nLenght of List: "+i);
         }
+    }
+    public static void delete(int key){
+        Node temp = head;
+        Node Btemp = null;
+        Node Ftemp = null;
+        if (temp != null && temp.data == key){
+            head = temp.next;
+            head.prev = null;
+            temp.next = null;
+            return;
+        }
+        while(temp != null && temp.data != key){
+            temp = temp.next;
+            Ftemp = temp.next;
 
+            System.out.println("Temp: "+temp.data);
+            Btemp = temp.prev;
+            System.out.println("Btemp: "+Btemp.data);
+            if (Ftemp == null){
+                Ftemp = temp;
+            }
+            System.out.println("Ftemp: "+Ftemp.data);
+        }
+        if (temp == null){
+            return;
+        }
+        System.out.println("Present: " + temp.data);
+//        System.out.println("Present: " + temp.prev.data);
+        System.out.println("Present: " + Btemp.next.data);
+        System.out.println("Present: " + Btemp.data);
+        System.out.println("Present: " + Ftemp.data);
+        if(Ftemp == temp){
+            Btemp.next = null;
+            tail = Btemp ;
+        }
+        else {
+            Ftemp.prev = temp.prev;
+            Btemp.next = temp.next;
+        }
     }
     public static void main(String[]args){
-        DoublyLinkedlist list = new DoublyLinkedlist();
+        deletion list = new deletion();
         list.Insert(1);
         list.Insert(2);
         list.Insert(3);
         list.Insert(4);
         list.Insert(5);
         list.Insert(6);
-        list.InsertatStart(0);
+        delete(6);
         Print();
     }
 }
